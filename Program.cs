9d66d6bd79bed4f5c86c08c460ea727b8ecb0f01
Valeriy Kovetskiy <kovetskiy.valeriy@gmail.com>
2019-05-21 20:18:55 +0300

    Demonstration methods

--- Program.cs
+++ Program.cs
@@ -9,10 +9,28 @@ class Program
             return 1;
         return x * Factorial(x - 1);
     }
-    public static void Main(string [] args)
+
+    private static IDownloader _downloader = new ImageDownloader();
+
+    private static void DownloadImageDemonstation()
+    {
+        string path = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg";
+        DownloadImageAsync(path);
+    }
+
+    private static async void DownloadImageAsync(string filePath)
+    {
+        await _downloader.DownloadAsync(new Uri(filePath));
+        Console.WriteLine($"{_downloader.Filename}");
+    }
+
+    public static void Main(string[] args)
     {
         System.Console.WriteLine("Hello world");
-        System.Console.WriteLine("Mish commit"); 
+        System.Console.WriteLine("Mish commit");
         System.Console.WriteLine(Factorial(5));
+
+        DownloadImageDemonstation();
+        Console.ReadKey();
     }
 }
