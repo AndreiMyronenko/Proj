80443bdb5f631c2e1bb2d18bfe0f5a60477b25a1
SenseiVik <senseyvik22@gmail.com>
2019-05-21 20:58:29 +0300

    Update Program.cs
    add extension class/method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
+static class Extention
+{
+    public static IEnumerable<T> DeserializeObjectCollection<T>(this string str)
+    {
+        List<T> result = null;
+
+        try
+        {
+            result = JsonConvert.DeserializeObject<List<T>>(str);
+        }
+        catch (JsonReaderException jrex)
+        {
+            throw jrex;
+        }
+
+        return result;
+    }
+}
 
 class Program
 {
@@ -14,5 +34,15 @@ class Program
         System.Console.WriteLine("Hello world");
         System.Console.WriteLine("Mish commit"); 
         System.Console.WriteLine(Factorial(5));
+
+        try
+        {
+            string text = "kek";
+            text.DeserializeObjectCollection<int>();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
