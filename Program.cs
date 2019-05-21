8fa1e2de971a52d448e0a5e8a95f046c9f7be1d8
bilayn <jura-bazak@yanex.ru>
2019-05-21 19:32:10 +0300

    Factorial

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@ using System;
 
 class Program
 {
-    static int 
+    static int Factorial(int x)
+    {
+        if (x == 0)
+            return 1;
+        return x * Factorial(x - 1);
+    }
     public static void Main(string [] args)
     {
         System.Console.WriteLine("Hello world");
