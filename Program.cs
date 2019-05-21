6bbbd010ee7d834fcde767d47b0ba7e2191189dc
shehurin <shehurin@gmail.com>
2019-05-21 20:34:09 +0300

    Finish Task

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 
-public class Student
+public interface IStudent
+{
+    string Name { get; set; }
+    string Surname { get; set; }
+    int Age { get; set; }
+}
+
+
+public class Student : IStudent
 {
     public string Name { get; set; }
     public string Surname { get; set; }
     public int Age { get; set; }
+
+
 }
 
 
@@ -17,10 +28,23 @@ class Program
             return 1;
         return x * Factorial(x - 1);
     }
-    public static void Main(string [] args)
+    public static void Main(string[] args)
     {
-        System.Console.WriteLine("Hello world");
-        System.Console.WriteLine("Mish commit"); 
-        System.Console.WriteLine(Factorial(5));
+        Student Valera = new Student { Name = "Valera", Surname = "Mashina", Age = 67 };
+        Student Bogdan = new Student { Name = "Bogdan", Surname = "GP", Age = 17 };
+        Student Sasha = new Student { Name = "Sasha", Surname = "Brat", Age = 18 };
+        Student Misha = new Student { Name = "Misha", Surname = "Mavashi", Age = 32 };
+        Student Kolya = new Student { Name = "Kolya", Surname = "Lesnikov", Age = 17 };
+        List<Student> students = new List<Student>();
+        students.Add(Valera);
+        students.Add(Bogdan);
+        students.Add(Sasha);
+        students.Add(Misha);
+        students.Add(Kolya);
+        foreach (Student student in students)
+        {
+            Console.WriteLine($"Name: {student.Name} \t Surname:{student.Surname}\t Age:{student.Age}");
+        }
+
     }
 }
