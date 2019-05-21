bf70de699a01701353ef103fe61b155205dcddbc
19Al85 <shapofall@gmail.com>
2019-05-21 21:37:17 +0300

    Fill Num

--- IntNum.cs
+++ IntNum.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    class IntNum
+    class IntNum : IInt
     {
+        public List<int> Nums => Nums;
+
+        public void Add(int n)
+        {
+            Nums.Add(n);
+        }
+
+        public void Remove(int n)
+        {
+            Nums.Remove(n);
+        }
     }
 }
