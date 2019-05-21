79cb04b7ec642bab717b40abebcfecc352d4442f
Valentin Semirenko <valentin.semirenko@gmail.com>
2019-05-21 21:15:41 +0300

    IStringList

--- Program.cs
+++ Program.cs
@@ -1,18 +1,67 @@
-using System;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-class Program
+namespace ConsoleApp20
 {
-    static int Factorial(int x)
+
+    class Program
     {
-        if (x == 0)
-            return 1;
-        return x * Factorial(x - 1);
-    }
-    public static void Main(string [] args)
-    {
-        System.Console.WriteLine("Hello world");
-        System.Console.WriteLine("Mish commit"); 
-        System.Console.WriteLine(Factorial(5));
+        static int Factorial(int x)
+        {
+            if (x == 0)
+                return 1;
+            return x * Factorial(x - 1);
+        }
+
+        class ListOfStrings : IStringList
+        {
+            List<string> stringList = new List<string>();
+
+            public void Add(string line)
+            {
+                stringList.Add(line);
+            }
+            public void Delete(string line)
+            {
+                stringList.Remove(line);
+            }
+
+            public IEnumerator<string> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print()
+            {
+                foreach (var i in stringList)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world");
+            System.Console.WriteLine("Mish commit");
+            System.Console.WriteLine(Factorial(5));
+
+            List<string> stringList = new List<string>() { "string1", "string2", "string3" };
+            stringList.Add("string4");
+            foreach (var i in stringList)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
     }
 }
