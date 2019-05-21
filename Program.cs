62698a06da1bfa6ff340aafba2557f980d1c53dd
Ololooo <demkovi@ukr.net>
2019-05-21 21:23:44 +0300

    Mawinka

--- Program.cs
+++ Program.cs
@@ -1,51 +1,8 @@
 using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-using System.IO;
+using Mawinka;
 
 
 
-public class Car
-{
-    public string Title { get; set; }
-    public string Color { get; set; }
-    public double Volume { get; set; }
-    public int Price { get; set; }
-    public static List<Car> GetCars()
-    {
-        return new List<Car>
-            {
-                new Car{ Title = "Porsche", Color = "Black", Price = 891374, Volume=4.5 },
-                new Car {Title="BMW", Volume = 3.2, Price=34134, Color="Blue" },
-                new Car{ Title = "Porsche", Color = "Black", Price = 891374, Volume=4.5 },
-                new Car {Title="BMW", Volume = 3.2, Price=34134, Color="Blue" },
-                new Car{ Title = "Porsche", Color = "Black", Price = 891374, Volume=4.5 },
-                new Car {Title="BMW", Volume = 3.2, Price=34134, Color="Blue" },
-                new Car{ Title = "Porsche", Color = "Black", Price = 891374, Volume=4.5 },
-                new Car{ Title = "Porsche", Color = "Black", Price = 891374, Volume=4.5 },
-                new Car{ Title = "Porsche", Color = "Black", Price = 891374, Volume=4.5 },
-                new Car{ Title = "Porsche", Color = "Black", Price = 891374, Volume=4.5 },
-                new Car {Title="BMW", Volume = 3.2, Price=34134, Color="Blue" },
-                new Car {Title="BMW", Volume = 3.2, Price=34134, Color="Blue" },
-                new Car {Title="BMW", Volume = 3.2, Price=34134, Color="Blue" },
-                new Car {Title="BMW", Volume = 3.2, Price=34134, Color="Blue" }
-            };
-    }
-
-    public static List<Car> LoadFromJson()
-    {
-        string res = File.ReadAllText("Save.txt");
-        List<Car> cars = new List<Car>();
-        cars = JsonConvert.DeserializeObject<List<Car>>(res);
-        return cars;
-    }
-
-    public static void SaveToJson(List<Car> cars)
-    {
-        string res = JsonConvert.SerializeObject(cars, Formatting.Indented);
-        File.WriteAllText("Save.txt", res);
-    }
-}
 
 
 class Program
